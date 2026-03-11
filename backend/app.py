92f2b4a99e1786c865a926734b6997d51776801e
mohd-musheer <musheerayan@gmail.com>
2026-03-11 17:10:53 +0530

    Cors Setup

--- backend/app.py
+++ backend/app.py
@@ -65,9 +65,16 @@ CORS(
     origins=[
         FRONTEND_URL,
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://sasuke-fruntend.onrender.com"
     ]
 )
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
 # =========================
 # Register Blueprints
